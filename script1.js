eb55fde28274f8d4477f7df4e627dd03241d3319
Shubham Dhobakle <97725662+shubhamdhobale@users.noreply.github.com>
2023-08-20 14:17:58 +0530

    git upload

---
+++ script1.js
@@ -0,0 +1,9 @@
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
